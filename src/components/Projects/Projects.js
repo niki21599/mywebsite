e2e3acd929c7293b56c7bd12b4ffc69fa5e4f47c
Niklas Burg <niklasburg1999.nb@gmail.com>
2023-03-08 15:17:52 +0100

    adjusted project section

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -10,8 +10,17 @@ const Projects = () => {
     >
       <h2 className="white headingProjects">My Work</h2>
       <ProjectCard
-        projectImg="/mywebsite/kanban.png"
+        projectImg="/mywebsite/videotube.jpg"
         reverse={false}
+        description="MyTube ermöglicht Benutzern das Durchsuchen und Anzeigen von Videos. Mit Hilfe von React und der YouTube-API können Benutzer auf eine große Auswahl an Videos zugreifen."
+        linkToProject="https://niki21599.github.io/youtube_clone/"
+        linkToGithub="https://github.com/niki21599/youtube_clone"
+        skills="React | Material UI | REST-API"
+        projectName="MyTube"
+      />
+      <ProjectCard
+        projectImg="/mywebsite/kanban.png"
+        reverse={true}
         description="MyKanban ist ein Kanban Board mit React Frontend und Django Backend, das eine effiziente Aufgabenverwaltung ermöglicht. Aufgaben können mit Drag-and-Drop-Funktionen einfach verschoben und organisiert werden."
         linkToProject="https://niki21599.github.io/kanban-frontend/"
         linkToGithub="https://github.com/niki21599/kanban-frontend"
@@ -20,30 +29,12 @@ const Projects = () => {
       />
       <ProjectCard
         projectImg="/mywebsite/chat2.0..jpg"
-        reverse={true}
+        reverse={false}
         description="Benutzer können mithilfe der intuitiven Benutzeroberfläche von MyChat einfach neue Chats starten und Nachrichten senden und empfangen. Das Django-Backend gewährleistet dabei eine zuverlässige Verarbeitung der Nachrichten und sorgt für eine reibungslose Benutzererfahrung."
         linkToProject="https://niki21599.github.io/chatFrontend/"
         linkToGithub="https://github.com/niki21599/chatFrontend"
         skills="React | Redux | Material UI | Django"
         projectName="MyChat"
-      />
-      <ProjectCard
-        projectImg="/mywebsite/crm.jpg"
-        reverse={false}
-        description="MyCRM bietet die Möglichkeit Kundendaten und Verkäufe zu speichern. Die gespeicherten Sales werden im Dashboard anschaulich visualisiert."
-        linkToProject="https://niki21599.github.io/react-crm/"
-        linkToGithub="https://github.com/niki21599/react-crm"
-        skills="React | Redux | Material UI | Django"
-        projectName="MyCRM"
-      />
-      <ProjectCard
-        projectImg="/mywebsite/videotube.jpg"
-        reverse={true}
-        description="MyTube ermöglicht Benutzern das Durchsuchen und Anzeigen von Videos. Mit Hilfe von React und der YouTube-API können Benutzer auf eine große Auswahl an Videos zugreifen."
-        linkToProject="https://niki21599.github.io/youtube_clone/"
-        linkToGithub="https://github.com/niki21599/youtube_clone"
-        skills="React | Material UI | REST-API"
-        projectName="MyTube"
       />
     </div>
   );
